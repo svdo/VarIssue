21375b865ba969a36e28d155369361d96beec598
Stefan van den Oord <soord@mac.com>
2019-05-05 21:09:14 +0200

    Adds a web view to the UI

--- App.js
+++ App.js
@@ -8,6 +8,7 @@
 
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
+import { WebView } from 'react-native-webview'
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -23,6 +24,7 @@ export default class App extends Component {
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <WebView source={{ html: '<h1>Hi WebView!</h1>' }} />
       </View>
     )
   }
@@ -32,7 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    // alignItems: 'center',
     backgroundColor: '#F5FCFF'
   },
   welcome: {
